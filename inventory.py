383f4370d32a2bb7e093a1b8bf5f5bb77ab55f6d
Oluwasola <113778568+sholashef@users.noreply.github.com>
2023-09-17 23:16:45 +0100

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,5 @@
+total_drinks = 100
+sold = input("How Many Drinks Have You Sold ? ")
+drinks_sold = int(sold)
+remaining_drinks = total_drinks - drinks_sold
+print("The reamining Stock : " + str(remaining_drinks))
